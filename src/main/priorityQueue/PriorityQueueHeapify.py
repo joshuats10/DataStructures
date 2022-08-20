f5befb03768eb7b43e94f730cabe5ce12be6eb13
joshuats10 <joshuats10@LAPTOP-H1134265.localdomain>
2022-08-20 22:03:00 +0900

    Commented PriorityQueueHeapify.

--- src/main/priorityQueue/PriorityQueueHeapify.py
+++ src/main/priorityQueue/PriorityQueueHeapify.py
@@ -1,11 +1,33 @@
+'''
+    A priority queue implementation using a binary heap that
+    able to do both min- and max-heap.
+
+    Input data: Node(info, priority)
+
+    @author Joshua Teguh Santoso, joshuateguhsantoso@gmail.com
+'''
+
 class Node:
+    """
+        This class acts as a container for the input data with
+        an info (can be anything but in this case name) with its priority level.
+    """
     def __init__(self, info, priority):
         self.info = info
         self.priority = priority
 
 class PriorityQueue:
+    """
+        This class is the implementation of priority queue using a binary heap
+        that able to do both min- and max-heap. 
+    """
     def __init__(self, prop, init_data=None):
+        
+        # Set the property of the priority queue
+        # whether it's a min- or max-heap.
         self.prop = prop
+
+        # Initialize the priority queue.
         if init_data:
             self.data = init_data
             for i in range((self.qSize()//2)-1, -1, -1):
@@ -13,53 +35,72 @@ class PriorityQueue:
         else:
             self.data = []
 
+    # Obtain the size of the priority queue
     def qSize(self):
         return len(self.data)
 
+    # Function to swap the data in the binary tree
     def swap(self, i, j):
         self.data[i], self.data[j] = self.data[j], self.data[i]
 
+    # Change the property of the priority queue from max- to min-heap
+    # or the other way around.
     def changeProperty(self, prop):
         self.prop = prop
         for i in range((self.qSize()//2)-1, -1, -1):
             self.heapify(i)
 
+    # Peek the root of the priority queue
     def peek(self):
         return self.data[0]
 
+    # Function to make the priority queue fulfills
+    # the binary heap condition
     def heapify(self, i):
         
         leftChild = 2 * i + 1
         rightChild = 2 * i + 2
         
         if self.prop == 'max':
-            largest = i
+            largest = i 
+
+            # See if left child of root exists and is greater than root
             if leftChild < self.qSize() and self.data[largest].priority < self.data[leftChild].priority:
                 largest = leftChild
+            
+            # See if right child of root exists and is greater than root
             if rightChild < self.qSize() and self.data[largest].priority < self.data[rightChild].priority:
                 largest = rightChild
 
+            # Swap root if needed
             if largest != i:
                 self.swap(i, largest)
                 self.heapify(largest)
 
         elif self.prop == 'min':
             smallest = i
+
+            # See if left child of root exists and is less than root
             if leftChild < self.qSize() and self.data[smallest].priority > self.data[leftChild].priority:
                 smallest = leftChild
-            if rightChild < self.qSize() and self.data[smallest].priority > self.data[rightChild].priority and self.data[rightChild].priority < self.data[leftChild].priority:
+
+            # See if right child of root exists and is less than root
+            if rightChild < self.qSize() and self.data[smallest].priority > self.data[rightChild].priority:
                 smallest = rightChild
 
+            # Swap root if needed
             if smallest != i:
                 self.swap(i, smallest)
                 self.heapify(smallest)
     
+    # Insert a new node into the priority queue
     def insert(self, new_node):
         self.data.append(new_node)
         if self.qSize() > 1:
             for i in range((self.qSize()//2)-1, -1, -1):
                 self.heapify(i)
 
+    # Extracting the max/min of the priority queue
     def pop(self):
         self.swap(0, -1)
         ret = self.data.pop(-1)
@@ -67,6 +108,7 @@ class PriorityQueue:
                 self.heapify(i)
         return (ret.info, ret.priority)
 
+    # Removing a specific data from the priority queue
     def remove(self, info):
         for i in range(self.qSize()):
             if info == self.data[i].info:
@@ -78,6 +120,7 @@ class PriorityQueue:
         for i in range((self.qSize()//2)-1, -1, -1):
                 self.heapify(i)
 
+    # Change the priority of a specific data in the priority queue
     def changePriority(self, info, new_priority):
         for i in range(self.qSize()):
             if info == self.data[i].info:
