dfb3d58713c4620c20aad58d96fffc486b080d48
joshuats10 <joshuats10@LAPTOP-H1134265.localdomain>
2022-08-26 02:55:50 +0900

    Add indexed priority queue.

---
+++ src/main/indexedPQ/indexedPQ.py
@@ -0,0 +1,101 @@
+"""
+    Indexed Priority Queue data structure implementation
+
+    @author Joshua Teguh Santoso, joshuateguhsantoso@gmail.com
+"""
+
+class indexedPQ:
+    def __init__(self, size):
+        self.val = [None] * size
+        self.pm = [None] * size
+        self.im = [None] * size
+        self.sz = 0
+
+    def size(self):
+        return self.sz
+
+    def isEmpty(self):
+        return self.sz == 0
+
+    def contains(self, ki):
+        return self.pm[ki] is not None
+
+    def insert(self, ki,  val):
+        self.val[ki] = val
+        self.pm[ki] = self.sz
+        self.im[self.sz] = ki
+
+        self.__swim(self.sz)
+        self.sz += 1
+
+    def remove(self, ki):
+        i = self.pm[ki]
+        self.sz -= 1
+        self.__swap(i, self.sz)
+
+        self.__sink(i)
+        self.__swim(i)
+
+        self.val[ki] = None
+        self.pm[ki] = None
+        self.im[self.sz] = None
+
+    def poll(self):
+        ret_ki = self.im[0]
+        ret_val = self.val[ret_ki]
+        self.remove(ret_ki)
+
+        return ret_ki, ret_val
+
+    def __swap(self, i, j):
+        self.pm[self.im[j]], self.pm[self.im[i]] = i, j
+        self.im[i], self.im[j] = self.im[j], self.im[i]
+
+    def __less(self, i, j):
+        return self.val[self.im[i]] < self.val[self.im[j]]
+
+    def __swim(self, i):
+        parent = (i-1)//2
+        while i > 0 and self.__less(i, parent):
+            self.__swap(i, parent)
+            i = parent
+            parent = (i-1)//2
+
+    def __sink(self, i):
+
+        while True:
+            left, right = 2 * i + 1, 2 * i + 2
+            smallest = left
+
+            if right < self.sz and self.__less(right, left):
+                smallest = right
+            
+            if left >= self.sz or self.__less(i, smallest):
+                break
+
+            self.__swap(smallest, i)
+            i = smallest
+
+if __name__ == "__main__":
+    lookup = {'Anna': 0, 'Bella': 1, 'Carly': 2, 'Dylan': 3,\
+              'Emily': 4, 'Fred': 5, 'George': 6, 'Henry': 7,\
+              'Isaac': 8, 'James': 9, 'Kelly': 10, 'Laura': 11
+    }
+    size = len(lookup)
+
+    val = [3,15,11,17,7,9,2,1,6,5,16,4]
+
+    ipq = indexedPQ(size+2)
+    for i in range(len(val)):
+        ipq.insert(i, val[i])
+
+    lookup[size] = 'Mary'
+    ipq.insert(size, 2)
+
+    ipq.poll()
+
+    ipq.remove(lookup['Laura'])
+    
+    print(ipq.val)
+    print(ipq.pm)
+    print(ipq.im)
